0861b7606829a3caefef616e99d8077bd9d320e7
Maria Ortega Rivas <mariorte@c1r9s6.42malaga.com>
2024-12-16 19:07:32 +0100

    Hecho hasta memchr

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariorte <mariorte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 18:56:32 by mariorte          #+#    #+#             */
+/*   Updated: 2024/12/16 19:06:53 by mariorte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (n > i)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((unsigned char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mariorte <mariorte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 17:21:00 by mariorte          #+#    #+#             */
-/*   Updated: 2024/12/16 18:53:13 by mariorte         ###   ########.fr       */
+/*   Updated: 2024/12/16 18:56:22 by mariorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
