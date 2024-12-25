2f50170ae789874b22a4064ca3fc800d94efe778
mariaorrri <m.ortega.rivas25@gmail.com>
2024-12-25 22:14:57 +0100

    algunos mas 25/12

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,5 +14,20 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+    	int		i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,5 +14,22 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	strat;
+	size_t	end;
+	char	*str;
+
+	str = 0;
+	if (s1 != 0 && set != 0)
+	{
+		strat = 0;
+		end = ft_strlen(s1);
+		while (s1[strat] && ft_strchr(set, s1[strat]))
+			strat++;
+		while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > strat)
+			end--;
+		str = (char *)malloc(sizeof(char) * (end - strat + 1));
+		if (str)
+			ft_strlcpy(str, &s1[strat], end - strat + 1);
+	}
+	return (str);
 }
