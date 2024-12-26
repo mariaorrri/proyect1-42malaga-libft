4db1aebf59fb821dfad3f59187e831777faa9824
Maria Ortega Rivas <mariorte@c1r10s2.42malaga.com>
2024-12-26 10:04:04 +0100

    Dos mas

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,33 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariorte <mariorte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/16 21:07:38 by mariorte          #+#    #+#             */
-/*   Updated: 2024/12/26 09:58:24 by mariorte         ###   ########.fr       */
+/*   Created: 2024/12/26 10:00:27 by mariorte          #+#    #+#             */
+/*   Updated: 2024/12/26 10:03:29 by mariorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    int		i;
+	int		i;
 	char	*str;
 
 	if (!s)
 		return (NULL);
-	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (!str)
-		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
-		str[i] = f(i, s[i]);
+		s[i] = f(i, s[i]);
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
+	return (s);
 }
